7c7fd86aec6d9a98af8edf5f1011ced608f58051
purajit <7026198+purajit@users.noreply.github.com>
2025-01-05 00:00:28 -0800

    add tests (#7)

--- src/cycle_detection.rs
+++ src/cycle_detection.rs
@@ -5,18 +5,25 @@ use std::collections::HashSet;
 
 pub(crate) fn detect_cycles() {
     let graph = super::ruff_util::ruff_graph(false, false, None);
+    let cycles = detect_cycles_in_graph(&graph);
+    for cycle in &cycles {
+        println!("{}", cycle.join(" -> "));
+    }
+    println!("{}", cycles.len())
+}
+
+fn detect_cycles_in_graph(graph: &HashMap<String, HashSet<String>>) -> HashSet<Vec<&str>> {
     let mut cycles = HashSet::new();
     for vertex in graph.keys() {
         cycles.extend(get_cycles_from_vertex(&graph, vertex));
     }
-
-    for cycle in &cycles {
-        println!("{}", cycle.join(" -> "));
-    }
-
-    println!("{}", cycles.len())
+    return cycles;
 }
 
+/// This is ported from pylint's cycle detection which is rather chaotic,
+/// and reasonably does not find all cycles (which is non-polynomial, of course)
+/// TODO: since we only care about minimal cycles, we could use Horton's Algorithm
+/// to find a minimum cycle basis in O(ve^3)
 fn get_cycles_from_vertex<'a>(
     graph: &'a HashMap<String, HashSet<String>>,
     vertex: &'a String,
@@ -52,4 +59,59 @@ fn get_cycles_from_vertex<'a>(
     }
 
     cycles
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    /// only one cycle in the graph
+    #[test]
+    fn test_detect_cycles_simple() {
+        let graph = HashMap::from([
+            ("a".to_string(), HashSet::from(["b".to_string()])),
+            ("b".to_string(), HashSet::from(["c".to_string()])),
+            ("c".to_string(), HashSet::from(["d".to_string()])),
+            ("c".to_string(), HashSet::from(["a".to_string()])),
+        ]);
+        assert_eq!(
+            detect_cycles_in_graph(&graph),
+            HashSet::from([vec!["a", "b", "c"]])
+        );
+    }
+
+    /// has many cycles - however, since the algorithm isn't guaranteed to find all
+    /// of them, we make sure we find at least a certain number - this is deterministic,
+    /// but not reasonable pre-calculable
+    #[test]
+    fn test_detect_cycles_complex() {
+        let graph = HashMap::from([
+            (
+                "a".to_string(),
+                HashSet::from([
+                    "b".to_string(),
+                    "j".to_string(),
+                    "k".to_string(),
+                    "n".to_string(),
+                    "q".to_string(),
+                    "r".to_string(),
+                ]),
+            ),
+            ("b".to_string(), HashSet::from(["a".to_string()])),
+            (
+                "j".to_string(),
+                HashSet::from(["a".to_string(), "k".to_string(), "l".to_string()]),
+            ),
+            ("k".to_string(), HashSet::from(["j".to_string()])),
+            ("l".to_string(), HashSet::from(["a".to_string()])),
+            ("n".to_string(), HashSet::from(["l".to_string()])),
+        ]);
+        // these are 3 cycles we know this finds; any changes to the logic could alter this
+        assert_eq!(
+            HashSet::from([vec!["a", "n", "l"], vec!["j", "k"], vec!["a", "b"]])
+                .difference(&detect_cycles_in_graph(&graph))
+                .count(),
+            0
+        );
+    }
 }
--- src/minimize_cycles.rs
+++ src/minimize_cycles.rs
@@ -1,13 +1,13 @@
 use std::collections::HashMap;
 use std::collections::HashSet;
 use std::fs;
-
 use std::vec::Vec;
 
-/// Gives the length of a cycle if it is shortened using an edge from vertex index i to j
+/// Gives the length of a cycle (number of nodes) if it is shortened
+/// using an edge from vertex index i to j
 fn cycle_size(c_len: usize, i: usize, j: usize) -> usize {
     if j > i {
-        c_len - (j - i + 1)
+        c_len - (j - i - 1)
     } else {
         i - j + 1
     }
@@ -27,11 +27,16 @@ pub(crate) fn canonical_cycle<'a>(c: &[&'a str]) -> Vec<&'a str> {
 
 /// Get the sub-cycle within c by using an edge from vertex index i to j
 fn sub_cycle<'a>(c: &Vec<&'a str>, i: usize, j: usize) -> Vec<&'a str> {
-    if i < j {
-        return c[..(i + 1)].iter().chain(&c[j..]).cloned().collect();
-    }
+    let new_cycle = if i < j {
+        &c[..(i + 1)]
+            .iter()
+            .chain(&c[j..])
+            .cloned()
+            .collect::<Vec<&str>>()
+    } else {
+        &c[j..(i + 1)]
+    };
 
-    let new_cycle = &c[j..(i + 1)];
     return canonical_cycle(new_cycle);
 }
 
@@ -115,5 +120,121 @@ pub(crate) fn minimize_cycles(cycles_results_file: String) {
 
     for cycle in &unique_minimal_cycles {
         println!("{}", cycle.join(" -> "));
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_cycle_size() {
+        // unchanged cycle
+        assert_eq!(cycle_size(10, 9, 0), 10);
+        // cycles changed by shortcuts
+        assert_eq!(cycle_size(10, 2, 5), 8);
+        assert_eq!(cycle_size(10, 0, 9), 2);
+        // cycles changed by a contained cycle
+        assert_eq!(cycle_size(10, 5, 2), 4);
+        assert_eq!(cycle_size(10, 5, 4), 2);
+        // not possible in reality, but single-node cycle
+        assert_eq!(cycle_size(10, 3, 3), 1);
+    }
+
+    #[test]
+    fn test_canonical_cycle() {
+        assert_eq!(canonical_cycle(&["a", "b"]), ["a", "b"]);
+        assert_eq!(canonical_cycle(&["b", "a"]), ["a", "b"]);
+        assert_eq!(canonical_cycle(&["b", "c", "a"]), ["a", "b", "c"]);
+        assert_eq!(
+            canonical_cycle(&["b", "c", "q", "a", "d"]),
+            ["a", "d", "b", "c", "q"]
+        );
+    }
+
+    #[test]
+    fn test_sub_cycle() {
+        // unchanged cycle
+        assert_eq!(sub_cycle(&vec!["a", "b", "c"], 2, 0), ["a", "b", "c"]);
+        // unchanged cycle, just canonicalized
+        assert_eq!(sub_cycle(&vec!["b", "c", "a"], 2, 0), ["a", "b", "c"]);
+        // shortcut
+        assert_eq!(sub_cycle(&vec!["a", "b", "c"], 1, 0), ["a", "b"]);
+        // should shortcut
+        assert_eq!(
+            sub_cycle(&vec!["b", "c", "e", "a", "d"], 2, 4),
+            ["b", "c", "e", "d"]
+        );
+        // should shortcut and canonicalize
+        assert_eq!(
+            sub_cycle(&vec!["b", "a", "c", "e", "d"], 1, 3),
+            ["a", "e", "d", "b"]
+        );
+        // should get contained cycle AND canonicalize
+        assert_eq!(sub_cycle(&vec!["b", "c", "a"], 2, 1), ["a", "c"]);
+    }
+
+    /// these have only one possible option, and just test reading edges from the graph
+    #[test]
+    fn test_minimize_cycle_simple() {
+        let graph = HashMap::from([("b".to_string(), HashSet::from(["a".to_string()]))]);
+        // unchanged cycle
+        assert_eq!(minimize_cycle(&graph, vec!["j", "k", "l"]), ["j", "k", "l"]);
+        assert_eq!(minimize_cycle(&graph, vec!["a", "j", "b"]), ["a", "j", "b"]);
+        // should shortcut
+        assert_eq!(minimize_cycle(&graph, vec!["a", "b", "c"]), ["a", "b"]);
+        assert_eq!(
+            minimize_cycle(&graph, vec!["b", "c", "e", "a", "d"]),
+            ["a", "d", "b"]
+        );
+        // should get contained cycle
+        assert_eq!(minimize_cycle(&graph, vec!["c", "a", "b", "d"]), ["a", "b"]);
+
+        // these have multiple options, and should find the best one
+    }
+
+    /// these have multiple options, and should find the best one
+    #[test]
+    fn test_minimize_cycle_complex() {
+        let graph = HashMap::from([
+            ("a".to_string(), HashSet::from([])),
+            ("b".to_string(), HashSet::from(["a".to_string()])),
+            (
+                "j".to_string(),
+                HashSet::from(["a".to_string(), "l".to_string()]),
+            ),
+            ("k".to_string(), HashSet::from(["j".to_string()])),
+            ("n".to_string(), HashSet::from(["l".to_string()])),
+        ]);
+        // three shortcuts: j -> a (cuts 3), b -> a (cuts 1)
+        assert_eq!(
+            minimize_cycle(&graph, vec!["a", "j", "k", "b", "l"]),
+            ["a", "j"]
+        );
+        // two shortcuts: k -> j (cuts 1), n -> l (cuts 2)
+        assert_eq!(
+            minimize_cycle(&graph, vec!["a", "k", "m", "n", "b1", "b2", "l"]),
+            ["a", "k", "m", "n", "l"]
+        );
+        // two contained cycles: l -> r -> j, and a -> b
+        assert_eq!(
+            minimize_cycle(&graph, vec!["q", "d", "l", "r", "j", "a", "b"]),
+            ["a", "b"]
+        );
+        // two contained cycles: l -> j, and a -> r -> b
+        assert_eq!(
+            minimize_cycle(&graph, vec!["q", "d", "l", "j", "a", "r", "b"]),
+            ["j", "l"]
+        );
+        // one shortcut: q -> a (cuts 6), one contained cycle: l -> l1 -> l2 -> j (cuts 2)
+        assert_eq!(
+            minimize_cycle(&graph, vec!["a", "q", "b", "l", "l1", "l2", "j"]),
+            ["a", "q", "b"]
+        );
+        // one shortcut: q -> a (cuts 3), one contained cycle: l -> l1 -> j (cuts 4)
+        assert_eq!(
+            minimize_cycle(&graph, vec!["a", "q1", "q", "b", "l", "l1", "j"]),
+            ["j", "l", "l1"]
+        );
     }
 }
